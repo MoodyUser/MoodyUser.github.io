3120856db027767dfd42fbd2dd28dacbf0afef16
Yoni Mood <yonispark@gmail.com>
2016-05-11 23:52:07 +0300

    bug fix and big decisions to unravel.

--- js/ang-control.js
+++ js/ang-control.js
@@ -7,38 +7,60 @@ var myApp = angular.module('myApp', []).factory('$exceptionHandler', function ()
         return exception;
     };
 });
+
+myApp.directive('well', function () {
+    return {
+        restrict: 'E',
+        replace: true,
+        scope: true,
+        link: function postLink(scope, iElement, iAttrs) {
+            jQuery(document).on('keypress', function (e) {
+                scope.$apply(scope.keyPressed(e));
+            });
+        }
+    };
+});
+
 myApp.controller('formCtrl', ['$scope', '$http', function ($scope, $http) {
-    $scope.form = {contain: "", title: "", from: "", to: "",orderby:"" };
+    $scope.form = {text: "", title: "", from: "", to: "", orderby: ""};
     $scope.relevantPosts = [];
     $http.get('data/data.json')
         .then(function (res) {
             $scope.posts = res.data;
             $scope.search();
         });
+
     $scope.search = function () {
         var whereStr = [];
         var relevant = Enumerable.From($scope.posts);
         if ($scope.form.title != "") {
             whereStr.push("$.title.toLowerCase().includes('" + $scope.form.title.toLowerCase() + "') ");
         }
-        if ($scope.form.contain != "") {
-            whereStr.push(" $.text.toLowerCase().includes('" + $scope.form.contain.toLowerCase() + "')");
+        if ($scope.form.text != "") {
+            whereStr.push(" $.text.toLowerCase().includes('" + $scope.form.text.toLowerCase() + "')");
         }
         if ($scope.form.from != "") {
-            whereStr.push(" new Date($.time) >  new Date('" +$scope.form.from + "')");
+            whereStr.push(" $.time >  new Date('" + $scope.form.from + "')");
         }
         if ($scope.form.to != "") {
-            whereStr.push(" new Date($.time) <  new Date('" +$scope.form.to + "')");
+            whereStr.push(" $.time <  new Date('" + $scope.form.to + "')");
         }
-        if (whereStr.length!=0) {
+        if (whereStr.length != 0) {
             relevant = relevant.Where(whereStr.join(" && "));
-                //.OrderBy("$."+$scope.form.orderby+"");
+            //.OrderBy("$."+$scope.form.orderby+"");
         }
-        $scope.relevantPosts = relevant.Select(function(x){
+        $scope.relevantPosts = relevant.Select(function (x) {
             x.readMore = true;
             return x;
         }).ToArray();
         $scope.$apply();
+    };
+
+    $scope.keyPressed = function (e) {
+        console.log(e.which == 96);
+        if (e.which == 96) {
+            console.log(e.which);
+        }
     };
 }]);
 
