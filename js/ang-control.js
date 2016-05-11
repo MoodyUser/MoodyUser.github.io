ce076e7456fc8c2c2a55bf3643cbbc2654af0ba3
Yoni Mood <yonispark@gmail.com>
2016-05-11 03:38:46 +0300

    styles and logo

--- js/ang-control.js
+++ js/ang-control.js
@@ -1,23 +1,20 @@
 /**
  * Created by Yoni Mood on 5/9/2016.
  */
-
 var myApp = angular.module('myApp', []).factory('$exceptionHandler', function () {
     return function (exception, cause) {
         exception.message += ' (caused by "' + cause + '")';
         return exception;
     };
 });
-
 myApp.controller('formCtrl', ['$scope', '$http', function ($scope, $http) {
-    $scope.form = {contain: "", title: "", from: "", to: ""};
+    $scope.form = {contain: "", title: "", from: "", to: "",orderby:"" };
     $scope.relevantPosts = [];
     $http.get('data/data.json')
         .then(function (res) {
             $scope.posts = res.data;
             $scope.search();
         });
-
     $scope.search = function () {
         var whereStr = [];
         var relevant = Enumerable.From($scope.posts);
@@ -33,12 +30,14 @@ myApp.controller('formCtrl', ['$scope', '$http', function ($scope, $http) {
         if ($scope.form.to != "") {
             whereStr.push(" new Date($.time) <  new Date('" +$scope.form.to + "')");
         }
-
         if (whereStr.length!=0) {
             relevant = relevant.Where(whereStr.join(" && "));
+                //.OrderBy("$."+$scope.form.orderby+"");
         }
-        //.OrderBy("$.user.screen_name")
-        $scope.relevantPosts = relevant.ToArray();
+        $scope.relevantPosts = relevant.Select(function(x){
+            x.readMore = true;
+            return x;
+        }).ToArray();
         $scope.$apply();
     };
 }]);
