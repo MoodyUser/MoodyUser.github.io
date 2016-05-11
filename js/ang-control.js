9bf31044f603c06acb09095f209e7be880a2dde7
Yoni Mood <yonispark@gmail.com>
2016-05-12 02:44:47 +0300

    bug fix, and basic ang-control tests added

--- js/ang-control.js
+++ js/ang-control.js
@@ -21,14 +21,32 @@ myApp.directive('well', function () {
     };
 });
 
+
+myApp.directive('test', function () {
+    return {
+        restrict: 'E',
+        replace: true,
+        scope: true,
+        link: function postLink(scope, iElement, iAttrs) {
+            scope.$apply(scope.basicTest());
+        }
+    };
+});
+
+
 myApp.controller('formCtrl', ['$scope', '$http', function ($scope, $http) {
     $scope.form = {text: "", title: "", from: "", to: "", orderby: ""};
     $scope.relevantPosts = [];
     $http.get('data/data.json')
         .then(function (res) {
             $scope.posts = res.data;
-            $scope.search();
+            $scope.searchApply();
         });
+
+    $scope.searchApply = function () {
+        $scope.search();
+        $scope.$apply();
+    };
 
     $scope.search = function () {
         var whereStr = [];
@@ -40,10 +58,10 @@ myApp.controller('formCtrl', ['$scope', '$http', function ($scope, $http) {
             whereStr.push(" $.text.toLowerCase().includes('" + $scope.form.text.toLowerCase() + "')");
         }
         if ($scope.form.from != "") {
-            whereStr.push(" $.time >  new Date('" + $scope.form.from + "')");
+            whereStr.push(" new Date($.time) >  new Date('" + $scope.form.from + "')");
         }
         if ($scope.form.to != "") {
-            whereStr.push(" $.time <  new Date('" + $scope.form.to + "')");
+            whereStr.push(" new Date($.time) <  new Date('" + $scope.form.to + "')");
         }
         if (whereStr.length != 0) {
             relevant = relevant.Where(whereStr.join(" && "));
@@ -51,9 +69,9 @@ myApp.controller('formCtrl', ['$scope', '$http', function ($scope, $http) {
         }
         $scope.relevantPosts = relevant.Select(function (x) {
             x.readMore = true;
+            x.time = new Date(x.time).toUTCString();
             return x;
         }).ToArray();
-        $scope.$apply();
     };
 
     $scope.keyPressed = function (e) {
@@ -62,5 +80,45 @@ myApp.controller('formCtrl', ['$scope', '$http', function ($scope, $http) {
             console.log(e.which);
         }
     };
+
+    $scope.basicTest = function () {
+        $http.get('data/data_test.json')
+            .then(function (res) {
+                var data = $scope.posts;
+                $scope.posts = res.data;
+
+                console.log("posts received: " + ($scope.posts.length == 1));
+                $scope.search();
+
+                console.log("relevantPosts is equal to posts: " + ($scope.posts.length == $scope.relevantPosts.length));
+                $scope.form.title = "checking title..";
+                $scope.search();
+                console.log("checking if nav.title works: " + (0 == $scope.relevantPosts.length));
+
+                $scope.form.title = "";
+                $scope.form.from = "11/11/2014 12:00 AM";
+                $scope.search();
+                console.log("checking if nav.from works: " + (1 == $scope.relevantPosts.length));
+
+                $scope.form.from = "";
+                $scope.form.to = "11/11/2017 12:00 AM";
+                $scope.search();
+                console.log("checking if nav.to works:" + (1 == $scope.relevantPosts.length));
+
+                $scope.form.to = "";
+                $scope.form.text = "not working....";
+                $scope.search();
+                console.log("checking if nav.text works:" + (0 == $scope.relevantPosts.length));
+                $scope.form.text = "";
+                console.log("");
+                console.log("basicTest ended..");
+
+                $scope.posts=data;
+                $scope.search();
+                $scope.$apply();
+            });
+    };
+
 }]);
+
 
